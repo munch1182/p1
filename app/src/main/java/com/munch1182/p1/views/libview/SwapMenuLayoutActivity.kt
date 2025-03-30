0759f354fed8e500640db835093c476d6dc9b6b3
munch1182 <munch1182@qq.com>
2025-03-31 00:34:06 +0800

    050324

--- app/src/main/java/com/munch1182/p1/views/libview/SwapMenuLayoutActivity.kt
+++ app/src/main/java/com/munch1182/p1/views/libview/SwapMenuLayoutActivity.kt
@@ -8,6 +8,7 @@ import androidx.customview.widget.ViewDragHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.munch1182.lib.base.toast
+import com.munch1182.lib.widget.SwapMenuLayout
 import com.munch1182.p1.base.BaseActivity
 import com.munch1182.p1.base.bind
 import com.munch1182.p1.databinding.ActivitySwapMenuLayoutBinding
@@ -25,29 +26,50 @@ class SwapMenuLayoutActivity : BaseActivity() {
         bind.asmlSml.setOnStateChangeListener { _, state -> if (state == ViewDragHelper.STATE_IDLE) toggleText() }
 
         bind.asmlRv.layoutManager = LinearLayoutManager(this)
-        bind.asmlRv.adapter = SMLAAdapter(bind.asmlRv)
+        val adapter = SMLAAdapter(bind.asmlRv)
+        bind.asmlRv.adapter = adapter
+
+        bind.asmlToggleRv.setOnClickListener {
+            //val index = Random.nextInt(0, 12)
+            //adapter.open(index)
+        }
+
+        bind.asmlUpdateRv.setOnClickListener {
+            //adapter.update("更新内容")
+        }
     }
 
     private fun toggleText() {
         bind.asmlToggle.text = if (bind.asmlSml.isOpen) "收起" else "展开"
     }
 
-    private class SMLAAdapter(rv: RecyclerView) : RecyclerView.Adapter<SMLAVH>() {
+    private class SMLAAdapter(private val rv: RecyclerView) : RecyclerView.Adapter<SMLAVH>() {
 
+        private var currOpen: SwapMenuLayout? = null
         private val conteClick = View.OnClickListener { toast("content click") }
         private val delClick = View.OnClickListener { toast("del click") }
+        private val list = MutableList(35) { "item $it" }
+        private val stateChange = SwapMenuLayout.OnStateChangeListener { view, state ->
+            if (state == ViewDragHelper.STATE_IDLE) {
+                if (view.isOpen) currOpen = view
+            } else if (currOpen != view) {
+                currOpen?.close()
+            }
+        }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SMLAVH {
             return SMLAVH(ItemSwapMenuLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
         }
 
-        override fun getItemCount(): Int {
-            return 55
-        }
+        override fun getItemCount() = list.size
 
         override fun onBindViewHolder(holder: SMLAVH, position: Int) {
+            val item = list[position]
+            holder.bind.ismlContentTv.text = item
             holder.bind.ismlContent1.setOnClickListener(conteClick)
             holder.bind.ismlDel.setOnClickListener(delClick)
+            holder.bind.ismlSml.tag = position
+            holder.bind.ismlSml.setOnStateChangeListener(stateChange)
         }
     }
 
