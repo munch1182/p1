70db6a6e216be61b8f5f56632c30e2195c1fe90f
munch1182 <munch1182@qq.com>
2025-10-23 16:49:07 +0800

    feat: impl notice.kt

--- app/src/main/java/com/munch1182/p1/views/ResultActivity.kt
+++ app/src/main/java/com/munch1182/p1/views/ResultActivity.kt
@@ -1,6 +1,7 @@
 package com.munch1182.p1.views
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Build
 import android.os.Bundle
 import androidx.compose.material3.Text
@@ -9,15 +10,20 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.lifecycleScope
+import com.munch1182.lib.AppHelper
 import com.munch1182.lib.base.appSetting
 import com.munch1182.lib.base.blueSetting
 import com.munch1182.lib.base.isBluetoothOpen
 import com.munch1182.lib.base.isGpsOpen
 import com.munch1182.lib.base.launchIO
 import com.munch1182.lib.base.locSetting
+import com.munch1182.lib.base.toDateStr
+import com.munch1182.lib.helper.NoticeHelper
 import com.munch1182.lib.helper.result.ifAll
 import com.munch1182.lib.helper.result.ifTrue
+import com.munch1182.lib.helper.result.isAllGranted
 import com.munch1182.lib.helper.result.judge
 import com.munch1182.lib.helper.result.manualIntent
 import com.munch1182.lib.helper.result.permission
@@ -27,6 +33,7 @@ import com.munch1182.p1.base.onPermission
 import com.munch1182.p1.ui.ClickButton
 import com.munch1182.p1.ui.SpacerV
 import com.munch1182.p1.ui.setContentWithRv
+import kotlinx.coroutines.delay
 
 class ResultActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,10 +49,7 @@ class ResultActivity : BaseActivity() {
         val callback: (Any) -> Unit = { result = it.toString() }
         ClickButton("相机权限") {
             callback("")
-            permission(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
-                .onPermission("相机" to "拍摄视频", "麦克风" to "录音和音频通话")
-                .manualIntent()
-                .request(callback)
+            permission(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO).onPermission("相机" to "拍摄视频", "麦克风" to "录音和音频通话").manualIntent().request(callback)
         }
         ClickButton("蓝牙权限") {
             callback("")
@@ -54,24 +58,59 @@ class ResultActivity : BaseActivity() {
                 permission.addAll(arrayOf(Manifest.permission.BLUETOOTH_SCAN, Manifest.permission.BLUETOOTH_CONNECT))
             }
             lifecycleScope.launchIO {
-                judge({ isBluetoothOpen() }, blueSetting())
-                    .onIntent("请前往蓝牙界面打开蓝牙，以使用蓝牙功能")
-                    .ifTrue()
-                    .permission(permission.toTypedArray())
-                    .onPermission("蓝牙" to "蓝牙相关功能")
-                    .manualIntent()
-                    .ifAll()
-                    .judge({ isGpsOpen() }, locSetting())
-                    .onIntent("请前往位置界面打开定位，以扫描附近蓝牙设备")
-                    .ifTrue()
-                    .permission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION))
-                    .onPermission("定位" to "扫描附近蓝牙设备")
-                    .manualIntent()
-                    .ifAll()
-                    .request(callback)
+                judge({ isBluetoothOpen() }, blueSetting()).onIntent("请前往蓝牙界面打开蓝牙，以使用蓝牙功能").ifTrue().permission(permission.toTypedArray()).onPermission("蓝牙" to "蓝牙相关功能").manualIntent().ifAll().judge({ isGpsOpen() }, locSetting())
+                    .onIntent("请前往位置界面打开定位，以扫描附近蓝牙设备").ifTrue().permission(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)).onPermission("定位" to "扫描附近蓝牙设备").manualIntent().ifAll().request(callback)
+            }
+        }
+        SpacerV()
+
+        ClickButton("发送通知") {
+            sendNotice(content = "现在是${System.currentTimeMillis().toDateStr()}")
+        }
+        ClickButton("发送进度") {
+            sendNotice(content = "0%") { id ->
+                lifecycleScope.launchIO {
+                    var i = 0
+                    while (i < 100) {
+                        sendNotice(notifyId = id, content = "${i}%")
+                        i += 10
+                        delay(500)
+                    }
+                }
             }
         }
 
         Text(result)
+    }
+
+}
+
+
+@SuppressLint("MissingPermission")
+private fun BaseActivity.sendNotice(title: String = "通知", content: String, notifyId: Int = NoticeHelper.newId, onSend: ((Int) -> Unit)? = null) {
+    val channelId = ".notice"
+    witNoticePermission(channelId) {
+        val res = NoticeHelper.send(
+            NotificationCompat.Builder(AppHelper, channelId).setSmallIcon(com.munch1182.p1.R.mipmap.ic_launcher).setContentTitle(title).setContentText(content).setAutoCancel(true),
+            notifyId = notifyId,
+        )
+        onSend?.invoke(res)
+    }
+}
+
+private fun BaseActivity.witNoticePermission(channelId: String, any: () -> Unit) {
+    val permission = {
+        val name = "测试通知"
+        NoticeHelper.checkOrCreateChannel(channelId, name, "测试通知")
+        judge({ !NoticeHelper.checkChannelIsDisable(channelId) }, appSetting())
+            .onIntent("该类型的通知已被关闭，请前往设置-通知管理-通知类别中手动允许${name}类别的通知")
+            .request { if (it) any() }
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        permission(Manifest.permission.POST_NOTIFICATIONS).onPermission("通知" to "获取通知").manualIntent().request {
+            if (it.isAllGranted()) permission()
+        }
+    } else {
+        permission()
     }
 }
